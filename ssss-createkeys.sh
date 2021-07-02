8a2a09321e416f7a49e8a9bfb35e375098c5e9fd
dbl-wemass <david.berrocal@wemass.com>
2021-07-02 14:11:08 +0200

    testeando con dos desencriptados

--- ssss-createkeys.sh
+++ ssss-createkeys.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 source ./config
 numTotalKeys=$(expr $NUMUSERS \* 2)
-neededKeys=$(expr $NUMUSERS + 1)
+#neededKeys=$(expr $NUMUSERS + 1)
+neededKeys=2
 rando=$(($RANDOM % $NUMUSERS))
+rando2=$(($RANDOM % $NUMUSERS))
 supersecret=$(cat superSecureId)
-totalKeys=($(time -p bin/ssss-split -t $neededKeys -n $numTotalKeys -w $RANDOM <<<$"${supersecret}\n"))
+totalKeys=($(time bin/ssss-split -t $neededKeys -n $numTotalKeys -w $RANDOM <<<$"${supersecret}\n"))
 sharedKeys=("${totalKeys[@]:0:$NUMUSERS}")
-privateKeys=("${totalKeys[@]:$NUMUSERS:$numTotalKeys}")
+#privateKeys=("${totalKeys[@]:$NUMUSERS:$numTotalKeys}")
+privateKeys=()
 echo "TotalKeys: ${#totalKeys[@]}; PrivateKeys: ${#privateKeys[@]}; SharedKeys: ${#sharedKeys[@]}; Necesarias para descifrar: $neededKeys"
 selectedKey=${sharedKeys[$rando]}
-legalDecrypt=("${privateKeys[@]}" $selectedKey)
+selectedKey2=${sharedKeys[$rando2]}
+legalDecrypt=()
+legalDecrypt=("${legalDecrypt[@]}" $selectedKey)
+legalDecrypt=("${legalDecrypt[@]}" $selectedKey2)
 echo $legalDecrypt >allkeys
+#printf "%s\n" "${totalKeys[@]}"
+echo "generadas ${#totalKeys[@]} claves"
 echo "intento de descifrado normal (${#privateKeys[@]} secretas + 1 publica: $rando; obtenidas: ${#legalDecrypt[@]})\n"
-time -p bin/ssss-combine -t $neededKeys <<<$(printf "%s\n" "${legalDecrypt[@]}")
-echo "worst Case Scenario: todos los vendors se ponen de acuerdo para intentar desencriptar la clave\n"
-time -p bin/ssss-combine -t $NUMUSERS <<<$(printf "%s\n" "${sharedKeys[@]}")
+time  bin/ssss-combine -t $neededKeys <<<$(printf "%s\n" "${legalDecrypt[@]}")
+#echo "worst Case Scenario: todos los vendors se ponen de acuerdo para intentar desencriptar la clave\n"
+#time -p bin/ssss-combine -t $NUMUSERS <<<$(printf "%s\n" "${sharedKeys[@]}")
